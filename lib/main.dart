7566d4e28ace98636047137544e62bdd4d23b199
Makdum <mkdmvx9@gmail.com>
2025-06-18 21:42:10 +0700

    UPDATE :  tambahkan splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:easy_pdf/screens/dashboard.dart';
+import 'package:easy_pdf/screens/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'easy PDF',
       debugShowCheckedModeBanner: false,
-      home: const Dashboard(),
+      home: const SplashScreen(),
     );
   }
 }
