e0a2e475e59ad332770bad88c078045167b37548
Makdum <mkdmvx9@gmail.com>
2025-06-19 21:31:50 +0700

    FEAT: tambah icon menu untuk sidebar & profile

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:easy_pdf/widgets/dashboard_widget/premium_banner.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
@@ -13,11 +14,39 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
       ),
       child: Row(
-        children: const [Icon(Icons.menu_outlined, color: Colors.white, size: 30,)],
+        children: [
+          // Toggle Sidebar
+          Icon(Icons.menu, size: 35),
+          Spacer(),
+
+          // Premium banner
+          PremiumBanner(),
+
+          const SizedBox(width: 12),
+
+          // *Profile
+          Container(
+            width: 35,
+            height: 35,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: ClipOval(
+              child: Image.asset(
+                'assets/my_image.png',
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Icon(Icons.person, size: 30, color: Colors.grey);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(80);
+  Size get preferredSize => const Size.fromHeight(90);
 }
