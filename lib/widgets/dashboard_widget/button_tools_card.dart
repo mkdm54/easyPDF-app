43c49180b5366feb445ce3d72b8eff87fac55003
Makdum <mkdmvx9@gmail.com>
2025-06-21 12:47:03 +0700

    FEAT : tambahkan daftar fungsi tombol tools pada dashboard

--- lib/widgets/dashboard_widget/button_tools_card.dart
+++ lib/widgets/dashboard_widget/button_tools_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easy_pdf/widgets/dashboard_widget/button_tools.dart';
+import 'package:easy_pdf/tools/jpg_to_pdf/jpg_to_pdf_tool.dart';
+// import 'package:easy_pdf/tools/edit_pdf/edit_pdf_tool.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class ButtonToolsCard extends StatefulWidget {
@@ -10,37 +12,76 @@ class ButtonToolsCard extends StatefulWidget {
 }
 
 class _ButtonToolsCardState extends State<ButtonToolsCard> {
-  final List<Map<String, dynamic>> listButtonTools = [
-    {
-      'icon': Icon(Icons.edit, color: Colors.orange[300], size: 35),
-      'label': 'Edit PDF',
-    },
-    {
-      'icon': Icon(Icons.picture_as_pdf, color: Colors.red, size: 35),
-      'label': 'JPG to PDF',
-    },
-    {
-      'icon': Icon(Icons.image, color: Colors.blue, size: 35),
-      'label': 'PDF to JPG',
-    },
-    {
-      'icon': Icon(Icons.compress, color: Colors.pink, size: 35),
-      'label': 'Compress Image',
-    },
-    {
-      'icon': Icon(Icons.camera_alt, color: Colors.green, size: 35),
-      'label': 'Scan to PDF',
-    },
-    {
-      'icon': SvgPicture.asset(
-        'assets/svg/table_merge_cells_icon.svg',
-        width: 35,
-        height: 35,
-        colorFilter: const ColorFilter.mode(Colors.purple, BlendMode.srcIn),
+  void _navigateToJpgToPdfPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const JpgToPdfTool()),
+    );
+  }
+
+  // void _navigateToEditPdfPage() {
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(builder: (context) => const EditPdfTool()),
+  //   );
+  // }
+
+  // Fungsi placeholder untuk tools lainnya
+  void _showComingSoon(String toolName) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$toolName - Coming Soon!'),
+        backgroundColor: Colors.blue,
+        duration: Duration(seconds: 2),
       ),
-      'label': 'Merge PDF',
-    },
-  ];
+    );
+  }
+
+  late final List<Map<String, dynamic>> listButtonTools;
+
+  @override
+  void initState() {
+    super.initState();
+
+    listButtonTools = [
+      {
+        'icon': Icon(Icons.edit, color: Colors.orange[300], size: 35),
+        'label': 'Edit PDF',
+        // 'onPressed': () => _navigateToEditPdfPage(),
+        'onPressed': () => _showComingSoon('edit PDF'),
+      },
+      {
+        'icon': Icon(Icons.picture_as_pdf, color: Colors.red, size: 35),
+        'label': 'JPG to PDF',
+        'onPressed': () => _navigateToJpgToPdfPage(),
+      },
+      {
+        'icon': Icon(Icons.image, color: Colors.blue, size: 35),
+        'label': 'PDF to JPG',
+        'onPressed': () => _showComingSoon('PDF to JPG'),
+      },
+      {
+        'icon': Icon(Icons.camera_alt, color: Colors.green, size: 35),
+        'label': 'Scan to PDF',
+        'onPressed': () => _showComingSoon('Scan to PDF'),
+      },
+      {
+        'icon': Icon(Icons.compress, color: Colors.pink, size: 35),
+        'label': 'Compress Image',
+        'onPressed': () => _showComingSoon('Compress Image'),
+      },
+      {
+        'icon': SvgPicture.asset(
+          'assets/svg/table_merge_cells_icon.svg',
+          width: 35,
+          height: 35,
+          colorFilter: const ColorFilter.mode(Colors.purple, BlendMode.srcIn),
+        ),
+        'label': 'Merge PDF',
+        'onPressed': () => _showComingSoon('Merge PDF'),
+      },
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +108,7 @@ class _ButtonToolsCardState extends State<ButtonToolsCard> {
                   return ButtonTools(
                     icon: item['icon'],
                     label: item['label'],
-                    onpressed: () {},
+                    onpressed: item['onPressed'],
                   );
                 }).toList(),
           ),
